5e43f6a5b9e7e07af9437fcd316c038f44943783
Syre Collins <dannyperfect235@gmail.com>
2025-03-08 06:07:43 -0800

    Update modelapi.py we bless jah

--- modelapi.py
+++ modelapi.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from typing import Any, Dict, List
 import pandas as pd
